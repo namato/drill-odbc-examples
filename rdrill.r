7736d4179e4d59769840706238ccbadc337bdce7
Nick Amato <amaton@gmail.com>
2015-07-22 18:22:22 -0700

    change to match updates from blog

--- rdrill.r
+++ rdrill.r
@@ -4,12 +4,13 @@ library(RODBC)
 ch <- odbcConnect("drill64")
 
 # run the query
-df = sqlQuery(ch, paste("SELECT columns[4] as state, COUNT(*) as ",
-    "countbystate from `dfs`.`root`.`air.csv` ",
-    " GROUP BY columns[4] ORDER BY countbystate DESC"))
+df = sqlQuery(ch, paste("SELECT columns[2] as state, COUNT(*) as ",
+                        "countbystate from `dfs`.`root`.`customers.all.csv` ",
+                        " GROUP BY columns[2] ORDER BY countbystate DESC"))
 df$perctotal <- df$countbystate / sum(df$countbystate) * 100
 
-df
+print(paste0("Total: ", sum(df$countbystate)))
+print(head(df, 10))
 
 # close the connection so we don't get a warning at the end
 odbcClose(ch)
